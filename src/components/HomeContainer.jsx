7f0204687043ee09e89b4d4e57a5d47fdf607eba
admin <project.ad2023@gmail.com>
2023-10-24 09:01:54 +0000

    Update 3 files

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -11,8 +11,8 @@ const HomeContainer = () => {
     >
       <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
         <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
-          <p className="text-base text-orange-500 font-semibold">Delivery</p>
-          <div className="w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
+          <p className="text-base text-orange-500 font-semibold">Chandigarh</p>
+          <div className="w-8 h-8 overflow-hidden drop-shadow-xl">
             <img
               src={Delivery}
               className="w-full h-full object-contain"
