9e49277339a9f819c246483a84b5038536f66e1f
admin <project.ad2023@gmail.com>
2023-11-26 14:45:19 +0000

    Update 7 files

--- src/utils/data.js
+++ src/utils/data.js
@@ -8,67 +8,92 @@ export const heroData = [
     id: 1,
     name: "Icecream",
     decp: "Chocolate & vanilla",
-    price: "5.25",
+    price: "79",
     imageSrc: I1,
   },
   {
     id: 2,
     name: "Strawberries",
     decp: "Fresh Strawberries",
-    price: "10.25",
+    price: "150",
     imageSrc: F1,
   },
   {
     id: 3,
     name: "Chicken Kebab",
     decp: "Mixed Kebab Plate",
-    price: "8.25",
+    price: "249",
     imageSrc: C3,
   },
   {
     id: 4,
     name: "Fish Kebab",
     decp: "Mixed Fish Kebab",
-    price: "5.25",
+    price: "200",
     imageSrc: Fi1,
   },
 ];
 
 export const categories = [
   {
+    id: 7,
+
+    name: "Bestseller",
+    urlParamName: "best",
+  },
+  {
+    id: 10,
+    name: "Groceries",
+    urlParamName: "gross",
+  },
+  {
     id: 1,
+    name: "Pizza",
+    urlParamName: "pizza",
+  },
+  {
+    id: 2,
     name: "Chicken",
     urlParamName: "chicken",
   },
   {
-    id: 2,
-    name: "Curry",
-    urlParamName: "curry",
-  },
-  {
     id: 3,
-    name: "Rice",
-    urlParamName: "rice",
+    name: "Rolls",
+    urlParamName: "roll",
   },
   {
     id: 4,
-    name: "Fish",
-    urlParamName: "fish",
+    name: "Paneer",
+    urlParamName: "paneer",
   },
   {
     id: 5,
-    name: "Fruits",
-    urlParamName: "fruits",
+    name: "Cake",
+    urlParamName: "cake",
   },
   {
     id: 6,
-    name: "Icecreams",
-    urlParamName: "icecreams",
+    name: "Chinese",
+    urlParamName: "chinese",
+  },
+
+  
+  {
+    id: 8,
+    name: "Burger",
+    urlParamName: "bet",
   },
 
   {
-    id: 7,
-    name: "Soft Drinks",
-    urlParamName: "drinks",
+    id: 9,
+    name: "South Indian",
+    urlParamName: "south",
+  },
+
+  
+  {
+    id: 11,
+    name: "Icecream",
+    urlParamName: "ice",
   },
 ];
